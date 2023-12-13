8a186bbcd49129a74c59d0e1dc4f1e9fec19f23d
Luvier Orjuela <luvierorjuela@gmail.com>
2023-12-13 17:57:28 -0500

    modificacion clase main v2

--- src/Zoologico/Main.java
+++ src/Zoologico/Main.java
@@ -6,7 +6,7 @@ public class Main {
     
     public static void main(String[] args) {
         
-        System.out.println("Zologico Departamental Main");
+        System.out.println("Zologico Departamental Main - Colombia");
         Animal misAnimales[] = new Animal[4];
         
         
